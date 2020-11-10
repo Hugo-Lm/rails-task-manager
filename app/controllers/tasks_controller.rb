84363815462e9e80cd5d9678269b17a3c62a6f9f
Hugo Le Morvan <lm.hugo@hotmail.fr>
2020-11-10 16:13:41 +0100

    Exercice RailsTaskManager terminé

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,47 @@
+class TasksController < ApplicationController
+  before_action :find_task, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    # @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.create(task_params)
+    redirect_to tasks_path
+  end
+
+  def edit
+    # @task = Task.find(params[:id])
+
+  end
+
+  def update
+    # @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to task_path(params[:id])
+  end
+
+  def destroy
+    # @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  def find_task
+    @task = Task.find(params[:id])
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+end
